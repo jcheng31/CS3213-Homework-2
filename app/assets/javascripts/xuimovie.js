09309eff11e0682dde51f59eec616d57a62850d5
Chen Zeyu <zeyufly@gmail.com>
2013-09-10 14:51:03 +0800

    c

---
+++ app/assets/javascripts/xuimovie.js
@@ -0,0 +1,13 @@
+window.Xuimovie = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Xuimovie.initialize();
+});

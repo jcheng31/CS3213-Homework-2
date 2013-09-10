77f0f3fc807bf357ce222bddfd711a60ae4fc579
Jerome Cheng <jcheng31@gmail.com>
2013-09-10 15:03:23 +0800

    Add basic models and collections.

--- app/assets/javascripts/xuimovie.js
+++ app/assets/javascripts/xuimovie.js
@@ -1,13 +1,48 @@
 window.Xuimovie = {
-  Models: {},
-  Collections: {},
-  Views: {},
-  Routers: {},
-  initialize: function() {
-    alert('Hello from Backbone!');
-  }
+	Models: {
+		Movie: Backbone.Model.extend({
+			defaults: {
+				id: 0,
+				summary: "",
+				title: "",
+				updated_at: "",
+				img_url: "",
+				user: {}
+			}
+		}),
+		Review: Backbone.Model.extend({
+			defaults: {
+				comment: "",
+				id: 0,
+				movie_id: 0,
+				score: 0,
+				updated_at: "",
+				user: {}
+			}
+		})
+	},
+	Collections: {
+		Movies: Backbone.Collections.extend({
+			url: '//cs3213.herokuapp.com/movies.json',
+			initialize: function() {
+				this.fetch();
+			}
+		}),
+		Reviews: Backbone.Collections.extend({
+
+		})
+	},
+	Views: {
+
+	},
+	Routers: {
+
+	},
+	initialize: function() {
+
+	}
 };
 
-$(document).ready(function(){
-  Xuimovie.initialize();
+$(document).ready(function() {
+	Xuimovie.initialize();
 });

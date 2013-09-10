8e267eb3aa0337f05aea43fe14b6c3ffd5430dbb
Chen Zeyu <zeyufly@gmail.com>
2013-09-10 13:58:06 +0800

    init rails app

---
+++ xuimovie/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xuimovie::Application.config.secret_key_base = '3228d2525aabb49e9cca55cdbdad89f06be9fd4ee4239ba4876a731c68519ba618f96456faaee90163a2cf0c23498742a9063f74e654a2af92b1b1ccdefcd9a5'
---
+++ xuimovie/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xuimovie::Application.config.session_store :cookie_store, key: '_xuimovie_session'

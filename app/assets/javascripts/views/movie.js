886834a69bf3c754bbbec69cce274c25420f6b43
Jerome Cheng <jcheng31@gmail.com>
2013-09-10 23:26:32 +0800

    Revert "Change movie view to replace its element, not add into it."
    This reverts commit adb214e85ce5fd95be254a52dc21c7d0a4b68c99.

--- app/assets/javascripts/views/movie.js
+++ app/assets/javascripts/views/movie.js
@@ -5,7 +5,7 @@ Xuimovie.Views.Movie = Backbone.View.extend({
 	},
 	render: function() {
 		var renderedHtml = this.template(this.model.attributes);
-		this.$el.replaceWith(renderedHtml);
+		this.$el.html(renderedHtml);
 		return this;
 	}
 });

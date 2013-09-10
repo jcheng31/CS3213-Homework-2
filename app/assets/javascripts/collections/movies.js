bcc533eacc3e03ec1ba8e13f0129bb757f08ec75
Jerome Cheng <jcheng31@gmail.com>
2013-09-10 22:32:11 +0800

    Remove parse function.

--- app/assets/javascripts/collections/movies.js
+++ app/assets/javascripts/collections/movies.js
@@ -2,9 +2,5 @@ Xuimovie.Collections.Movies = Backbone.Collection.extend({
 	url: '//cs3213.herokuapp.com/movies.json',
 	model: Xuimovie.Models.Movie,
 	initialize: function() {
-	},
-	parse: function(response) {
-		console.log(response);
-		return response;
 	}
 });

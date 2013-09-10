71a44a2a66fc22fc5c3d1619b57fd1b77a8d9e33
Jerome Cheng <jcheng31@gmail.com>
2013-09-10 18:10:04 +0800

    Add model to Movies collection.

--- app/assets/javascripts/xuimovie.js
+++ app/assets/javascripts/xuimovie.js
@@ -24,6 +24,7 @@ window.Xuimovie = {
 	Collections: {
 		Movies: Backbone.Collection.extend({
 			url: '//cs3213.herokuapp.com/movies.json',
+			model: Xuimovie.Models.Movie,
 			initialize: function() {
 				this.fetch();
 			}

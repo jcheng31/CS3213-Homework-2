3dfd6da23c66ed4cf4c692b3079e211be3cbc258
Tony Le <lvtien.dev@gmail.com>
2013-09-11 09:27:40 +0800

    Redirect url based on env

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
+  def redirect_url
+    return 'http://localhost:3000/redirect' if Rails.env == "development" 
+    'http://xui-movies.herokuapp.com/redirect'
+  end
+
 	def client
 		client_id = "6e031aa4ed74f5b3f3141de7d99c5444"
   	client_secret = "7dc8df716c2915fdeaec773cd390f599"
@@ -6,13 +11,13 @@ class SessionsController < ApplicationController
 	end
 
   def new
-  	redirect_to client.auth_code.authorize_url(:authorize_url => '/oauth/new', :redirect_uri => 'http://localhost:3000/redirect')
+  	redirect_to client.auth_code.authorize_url(:authorize_url => '/oauth/new', :redirect_uri => redirect_url)
   end
 
   def redirect
-    access_token = client.auth_code.get_token(params[:code], :redirect_uri => 'http://localhost:3000')
+    access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_url)
     session[:access_token] = access_token.token
-    puts session[:access_token]
+    puts "Access Token: " + session[:access_token]
     redirect_to '/'
   end
 

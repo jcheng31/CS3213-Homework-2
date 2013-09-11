dde652dbb2c64e58f716e7b30d7963af6bf1865c
Tony Le <lvtien.dev@gmail.com>
2013-09-11 09:17:27 +0800

    Delete unused endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Xuimovie::Application.routes.draw do
-  get "test/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

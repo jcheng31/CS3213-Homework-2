1f20e5cdcedf19f906864b3f63e39336f6eda0a5
Chen Zeyu <zeyufly@gmail.com>
2013-09-11 19:02:29 +0800

    add view movie button

--- app/assets/javascripts/views/movie.js
+++ app/assets/javascripts/views/movie.js
@@ -1,5 +1,7 @@
 Xuimovie.Views.Movie = Backbone.View.extend({
 	template: JST["movie"],
+  tagName: "li",
+  className: "span4",
 	initialize: function() {
 		this.render();
 	},
@@ -7,5 +9,11 @@ Xuimovie.Views.Movie = Backbone.View.extend({
 		var renderedHtml = this.template(this.model.attributes);
 		this.$el.html(renderedHtml);
 		return this;
-	}
+	},
+  events:{
+    "click button#viewMovie": "viewMovieDetail"
+  },
+  viewMovieDetail: function(){
+    alert("viewMovie !!!!");
+  }
 });

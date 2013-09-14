47c1205fa6aac17825536c1f0dc291068f8fe11f
Jerome Cheng <jcheng31@gmail.com>
2013-09-14 18:16:10 +0800

    Indentation.

--- app/assets/javascripts/views/movie.js
+++ app/assets/javascripts/views/movie.js
@@ -1,20 +1,22 @@
 Xuimovie.Views.Movie = Backbone.View.extend({
 	template: JST["movie"],
-  tagName: "li",
-  className: "span4",
+	tagName: "li",
+	className: "span4",
 	initialize: function() {
 		this.render();
 	},
 	render: function() {
 		var renderedHtml = this.template(this.model.attributes);
 		this.$el.html(renderedHtml);
-    this.delegateEvents();
+		this.delegateEvents();
 		return this;
 	},
-  events:{
-    "click img#viewMovie": "viewMovieDetail"
-  },
-  viewMovieDetail: function(){
-    mainRouter.navigate("movies/" + this.model.attributes.id,{trigger: true});
-  }
+	events: {
+		"click img#viewMovie": "viewMovieDetail"
+	},
+	viewMovieDetail: function() {
+		mainRouter.navigate("movies/" + this.model.attributes.id, {
+			trigger: true
+		});
+	}
 });

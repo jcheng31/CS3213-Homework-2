f19ac89fa64ce4467a10f53dce8d9af53e5ae161
Tony Le <lvtien.dev@gmail.com>
2013-09-14 20:14:44 +0800

    Create Movie first attempt

--- app/assets/javascripts/views/movieCreate.js
+++ app/assets/javascripts/views/movieCreate.js
@@ -8,9 +8,25 @@ Xuimovie.Views.MovieCreate = Backbone.View.extend({
 		return this;
 	},
   events:{
-    "click #save_movie": "createMovie"
+    "click #submit": "createMovie"
   },
   createMovie: function(e) {
-  	alert("CLICKED");
+  	var data = { 	'access_token': gon.token,
+			       	 		'movie': {
+			       	 			'title': $('#movie_name').val(),
+			          		'summary': $('#movie_summary').val(),
+			          		'img': $('#movie_img').val()
+			        		}
+			       	 };
+		console.log(data);
+		e.preventDefault();
+		var url = 'http://cs3213.herokuapp.com/movies.json';
+		if (typeof(gon) != 'undefined') {
+			$.post(url, data).done(function(data, success) {
+			});
+			// mainRouter.navigate("/", { trigger: true });
+		} else {
+			alert("Please login first :)");
+		}
   }
 });

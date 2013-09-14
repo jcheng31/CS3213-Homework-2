29067efdd55c8096ac175c9614fb7ecebd507ea3
Tony Le <lvtien.dev@gmail.com>
2013-09-14 21:58:55 +0800

    Add back button

--- app/assets/javascripts/views/movieDetail.js
+++ app/assets/javascripts/views/movieDetail.js
@@ -3,13 +3,17 @@ Xuimovie.Views.MovieDetail = Backbone.View.extend({
 	initialize: function() {
 		this.render();
 	},
+	events: {
+		'click #back-list-btn': 'back'
+	},
 	render: function() {
 		var renderedHtml = this.template(this.model.attributes);
 		this.$el.html(renderedHtml);
     this.delegateEvents();
 		return this;
 	},
-  events:{
-    //"click button#viewMovie": "viewMovieDetail"
-  },
+ 	back: function(e) {
+ 		e.preventDefault();
+ 		mainRouter.navigate('/', { trigger: true });
+ 	}
 });

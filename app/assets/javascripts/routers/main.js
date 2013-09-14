ae4d30206716f0184e1f3ddd9570aa023bd150ae
Jerome Cheng <jcheng31@gmail.com>
2013-09-14 17:50:20 +0800

    Properly set URL for pagination.

--- app/assets/javascripts/routers/main.js
+++ app/assets/javascripts/routers/main.js
@@ -1,44 +1,49 @@
 Xuimovie.Routers.Main = Backbone.Router.extend({
   routes: {
-    "" : "mainPage",
-    "loggedout" : "redirectToMain",
-    "movies/:id" : "getMovie",
-    "new_movie" : "createMovie",
-    ":moviePage" : "mainPage"
+    "": "mainPage",
+    "loggedout": "redirectToMain",
+    "movies/:id": "getMovie",
+    "new_movie": "createMovie",
+    ":moviePage": "mainPage"
   },
 
   mainPage: function(moviePage) {
-    var movieCollection = new Xuimovie.Collections.Movies();
-
     if (!moviePage) {
       moviePage = 1;
     }
+
+
+    var movieCollection = new Xuimovie.Collections.Movies({
+      url: '//cs3213.herokuapp.com/movies.json?page=' + moviePage
+    });
+
     mainView = new Xuimovie.Views.Movies({
       el: document.getElementById("application-content"),
       collection: movieCollection,
-      url: '//cs3213.herokuapp.com/movies.json?page=' + moviePage
     });
     movieCollection.fetch();
   },
 
   redirectToMain: function() {
-    this.navigate("/", {trigger: true});
+    this.navigate("/", {
+      trigger: true
+    });
   },
 
-  getMovie: function(id){
+  getMovie: function(id) {
     var movie = new Xuimovie.Models.Movie();
     movie.id = id + ".json";
     movie.urlRoot = '//cs3213.herokuapp.com/movies';
     movie.fetch({
-      success: function(){
+      success: function() {
         movieDetailView = new Xuimovie.Views.MovieDetail({
           el: document.getElementById("application-content"),
-          model:movie
+          model: movie
         });
         // movieDetailView.render();
-        
-        var url = "//cs3213.herokuapp.com/movies/"+ id + "/reviews.json";
-        var reviewCollection = new Xuimovie.Collections.Reviews([],{
+
+        var url = "//cs3213.herokuapp.com/movies/" + id + "/reviews.json";
+        var reviewCollection = new Xuimovie.Collections.Reviews([], {
           url: url,
           movie_id: id
         });

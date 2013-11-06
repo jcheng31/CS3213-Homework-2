2824870e38c0992f2e2f666708d5cb4bfe585b51
Jerome Cheng <jcheng31@gmail.com>
2013-11-06 13:34:22 +0800

    Fix incorrect reference to Underscore.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require underscore
+//= require underscore-min
 //= require xui
 //= require xuimovie
 //= require jquery.form

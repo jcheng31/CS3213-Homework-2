65b43406b551bc11366c42f1ced5eba0936e7a78
Jerome Cheng <jcheng31@gmail.com>
2013-11-07 10:29:25 +0800

    Indentation.

--- app/assets/javascripts/xui.js
+++ app/assets/javascripts/xui.js
@@ -823,77 +823,77 @@ Xui.sync = function (method, model, options) {
 };
 
 // Xui.Router
-var Router = Xui.Router = function(options) {
-    options || (options = {});
-    if (options.routes) this.routes = options.routes;
-    this._bindRoutes();
-    this.initialize.apply(this, arguments);
-  };
+var Router = Xui.Router = function (options) {
+  options || (options = {});
+  if (options.routes) this.routes = options.routes;
+  this._bindRoutes();
+  this.initialize.apply(this, arguments);
+};
 
-  var optionalParam = /\((.*?)\)/g;
-  var namedParam = /(\(\?)?:\w+/g;
-  var splatParam = /\*\w+/g;
-  var escapeRegExp = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+var optionalParam = /\((.*?)\)/g;
+var namedParam = /(\(\?)?:\w+/g;
+var splatParam = /\*\w+/g;
+var escapeRegExp = /[\-{}\[\]+?.,\\\^$|#\s]/g;
 
-  //extend router with Events
-  _.extend(Router.prototype, Xui.Events, {
+//extend router with Events
+_.extend(Router.prototype, Xui.Events, {
 
-    initialize: function(){},
+  initialize: function () {},
 
-    // this.route('search/:query/p:num', 'search', function(query, num) {
-    route: function(route, name, callback) {
-      if (!_.isRegExp(route)) route = this._routeToRegExp(route);
-      if (_.isFunction(name)) {
-        callback = name;
-        name = '';
-      }
-      if (!callback) callback = this[name];
-      var router = this;
-      Xui.history.route(route, function(fragment) {
-        var args = router._extractParameters(route, fragment);
-        callback && callback.apply(router, args);
-        router.trigger.apply(router, ['route:' + name].concat(args));
-        router.trigger('route', name, args);
-        Xui.history.trigger('route', router, name, args);
-      });
-      return this;
-    },
-
-    // proxy to `Xui.history` to save a fragment into the history.
-    navigate: function(fragment, options) {
-      Xui.history.navigate(fragment, options);
-      return this;
-    },
-
-    _bindRoutes: function() {
-      if (!this.routes) return;
-      this.routes = _.result(this, 'routes');
-      var route, routes = _.keys(this.routes);
-      while ((route = routes.pop()) != null) {
-        this.route(route, this.routes[route]);
-      }
-    },
-
-    _routeToRegExp: function(route) {
-      route = route.replace(escapeRegExp, '\\$&')
-                   .replace(optionalParam, '(?:$1)?')
-                   .replace(namedParam, function(match, optional){
-                     return optional ? match : '([^\/]+)';
-                   })
-                   .replace(splatParam, '(.*?)');
-      return new RegExp('^' + route + '$');
-    },
-
-    _extractParameters: function(route, fragment) {
-      var params = route.exec(fragment).slice(1);
-      return _.map(params, function(param) {
-        return param ? decodeURIComponent(param) : null;
-      });
+  // this.route('search/:query/p:num', 'search', function(query, num) {
+  route: function (route, name, callback) {
+    if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+    if (_.isFunction(name)) {
+      callback = name;
+      name = '';
     }
+    if (!callback) callback = this[name];
+    var router = this;
+    Xui.history.route(route, function (fragment) {
+      var args = router._extractParameters(route, fragment);
+      callback && callback.apply(router, args);
+      router.trigger.apply(router, ['route:' + name].concat(args));
+      router.trigger('route', name, args);
+      Xui.history.trigger('route', router, name, args);
+    });
+    return this;
+  },
 
-  });
+  // proxy to `Xui.history` to save a fragment into the history.
+  navigate: function (fragment, options) {
+    Xui.history.navigate(fragment, options);
+    return this;
+  },
 
-var History = Xui.History = function() {
+  _bindRoutes: function () {
+    if (!this.routes) return;
+    this.routes = _.result(this, 'routes');
+    var route, routes = _.keys(this.routes);
+    while ((route = routes.pop()) != null) {
+      this.route(route, this.routes[route]);
+    }
+  },
+
+  _routeToRegExp: function (route) {
+    route = route.replace(escapeRegExp, '\\$&')
+      .replace(optionalParam, '(?:$1)?')
+      .replace(namedParam, function (match, optional) {
+        return optional ? match : '([^\/]+)';
+      })
+      .replace(splatParam, '(.*?)');
+    return new RegExp('^' + route + '$');
+  },
+
+  _extractParameters: function (route, fragment) {
+    var params = route.exec(fragment).slice(1);
+    return _.map(params, function (param) {
+      return param ? decodeURIComponent(param) : null;
+    });
+  }
+
+});
+
+var History = Xui.History = function () {
   // Store all handlers for given URL regex
   this.handlers = [];
   _.bindAll(this, 'checkUrl');
@@ -921,7 +921,7 @@ History.started = false;
 
 _.extend(History.prototype, Events, {
   // Get the hash value of url
-  getHash: function(window) {
+  getHash: function (window) {
     var match = (window || this).location.href.match(/#(.*)$/);
     return match ? match[1] : '';
   },
@@ -942,16 +942,18 @@ _.extend(History.prototype, Events, {
 
   // Start the handling whenever hash value changes.
   // Return true if the current URL matches a handler, false otherwise
-  start: function(options) {
+  start: function (options) {
     if (History.started) throw new Error("History has started already");
     History.started = true;
 
     // Initial configuration
-    this.options = _.extend({root: '/'}, this.options, options);
+    this.options = _.extend({
+      root: '/'
+    }, this.options, options);
     this.root = this.options.root;
     this._wantsHashChange = this.options.hashChange !== false;
-    this._wantsPushState = !!this.options.pushState;
-    this._hasPushState = !!(this.options.pushState && this.history && this.history.pushState);
+    this._wantsPushState = !! this.options.pushState;
+    this._hasPushState = !! (this.options.pushState && this.history && this.history.pushState);
     var fragment = this.getFragment();
 
     // Normalize root to always include a leading and trailing slash.
@@ -989,12 +991,15 @@ _.extend(History.prototype, Events, {
   },
 
   // Add a route regex to handlers. May override the old one
-  route: function(route, callback) {
-    this.handlers.unshift({route: route, callback: callback});
+  route: function (route, callback) {
+    this.handlers.unshift({
+      route: route,
+      callback: callback
+    });
   },
 
   // Check if the currnet url has changed to call loadUrl
-  checkUrl: function(e) {
+  checkUrl: function (e) {
     var current = this.getFragment();
     if (current === this.fragment) return false;
     this.loadUrl();
@@ -1003,9 +1008,9 @@ _.extend(History.prototype, Events, {
 
   // Find and apply to matched handlers for current fragment
   // Return true if found, false otherwise
-  loadUrl: function(fragment) {
+  loadUrl: function (fragment) {
     fragment = this.fragment = this.getFragment(fragment);
-    return _.any(this.handlers, function(handler) {
+    return _.any(this.handlers, function (handler) {
       if (handler.route.test(fragment)) {
         handler.callback(fragment);
         return true;
@@ -1014,9 +1019,11 @@ _.extend(History.prototype, Events, {
   },
 
   // Save a fragment into hash history, or replace URL if trigger: true is passed
-  navigate: function(fragment, options) {
+  navigate: function (fragment, options) {
     if (!History.started) return false;
-    if (!options || options === true) options = {trigger: !!options};
+    if (!options || options === true) options = {
+      trigger: !! options
+    };
 
     // If trigger : true, the route callback will fired
     // If replace : true, replace URL without adding into history
@@ -1027,7 +1034,7 @@ _.extend(History.prototype, Events, {
     fragment = fragment.replace(pathStripper, '');
     if (this.fragment === fragment) return;
     this.fragment = fragment;
-    
+
     // Remove trailing slash on the root
     if (fragment === '' && url === '/') url = url.slice(0, -1);
 
@@ -1050,7 +1057,7 @@ _.extend(History.prototype, Events, {
   },
   // Update the hash location, either replacing the current entry, 
   // or adding a new one to the browser history.
-  _updateHash: function(location, fragment, replace) {
+  _updateHash: function (location, fragment, replace) {
     if (replace) {
       var href = location.href.replace(/(javascript:|#).*$/, '');
       location.replace(href + '#' + fragment);
